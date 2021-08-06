dbb95f7ac609a2f445dee75ef835a5bc453c776b
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-06 22:07:34 +0900

    create setting menu navigation

--- duncheck_client/src/container/Setting.js
+++ duncheck_client/src/container/Setting.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Setting = () => {
     return( 
         <>
-            setting
+            <div className = 'setting-cut-box'>
+                <ul className = 'setting-menu-navigation'>
+                    <li className = 'setting-menu'>장비</li>
+                    <li className = 'setting-menu'>딜표</li>
+                    <li className = 'setting-menu'>버프력</li>
+                </ul>
+            </div>
         </>
     )
 }
