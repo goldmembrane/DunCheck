80ffe798355ba0c86b91f1ca5878eababc2e5d43
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-10 15:34:45 +0900

    create buffScore.js mockup

--- duncheck_client/src/container/setting/BuffScore.js
+++ duncheck_client/src/container/setting/BuffScore.js
@@ -3,7 +3,14 @@ import React from 'react'
 const BuffScore = () => {
     return(
         <>
-            BuffScore
+            <div className = 'buff-cut-title-box'>
+                <div className = 'buff-cut-title'>버프력 컷 설정</div>
+                <div className = 'buff-cut-container'>
+                    <span>버프력 : </span>
+                    <input type = 'number' className = 'buff-cut' />
+                    <button className = 'buff-cut-setting'>설정</button>
+                </div>
+            </div>
         </>
     )
 }
