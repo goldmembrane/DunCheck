c427b3717b50ee04363586c13ddf0ea2ca9ec910
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-10 20:28:01 +0900

    create Avatar.js, Creature.js and Talisman.js mockup

--- duncheck_client/src/container/Equipment/Avatar.js
+++ duncheck_client/src/container/Equipment/Avatar.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Avatar = () => {
     return (
         <>
-            Avatar
+            <div className = 'avatar-cut-box'>
+                <div className = 'avatar-cut-title'>아바타 컷 설정</div>
+                <span>딜 플티 체크 여부 : </span>
+                <input type = 'checkbox'className = 'avatar-check' />
+                <button className = 'avatar-cut-setting'>설정</button>
+            </div>
         </>
     )
 }
--- duncheck_client/src/container/Equipment/Talisman.js
+++ duncheck_client/src/container/Equipment/Talisman.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Talisman = () => {
     return (
         <>
-            Talisman
+            <div className = 'talisman-cut-box'>
+                <div className = 'talisman-cut-title'>탈리스만 컷 설정</div>
+                <span>올 카펠라 체크 여부 : </span>
+                <input type = 'checkbox' className = 'talisman-cut-check' />
+                <button className = 'talisman-cut-setting'>설정</button>
+            </div>
         </>
     )
 }
