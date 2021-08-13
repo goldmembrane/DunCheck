68d2606ac531bbda9c94c9c6fcacef8c2f88540e
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-13 22:24:55 +0900

    install react-bootstrap for using dropdown and apply at Creature.js

--- duncheck_client/src/container/Equipment/Creature.js
+++ duncheck_client/src/container/Equipment/Creature.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import { useDispatch } from 'react-redux'
+import { settingCreatureCut } from '../../action/CreatureAction'
 
 const Creature = () => {
+
+    const [creature, setCreature] = useState('')
+
+    const dispatch = useDispatch()
+
+    const onCreatureHandler = (e) => {
+        setCreature(e)
+    }
+
+    const onSubmitCreatureHandler = (e) => {
+        e.preventDefault()
+        let creatureBody = {
+            creature: creature
+        }
+
+        dispatch(settingCreatureCut(creatureBody))
+    }
     return (
         <>
             <div className = 'creature-cut-box'>
                 <div className = 'creature-title'>크리쳐 컷 설정</div>
-                <span>크리쳐 : </span>
-                <div className = 'creature-dropdown'>크리쳐 드롭다운</div>
-                <button className = 'creature-cut-setting'>설정</button>
+                <form
+                    onSubmit = {onSubmitCreatureHandler}>
+                    <span>크리쳐 : </span>
+                    <div className = 'creature-dropdown'>
+                        <Dropdown
+                            onSelect = {onCreatureHandler}>
+                            <Dropdown.Toggle id = 'creature-dropdown'>
+                                크리쳐를 선택하세요
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item eventKey = '10%'>10%</Dropdown.Item>
+                                <Dropdown.Item eventKey = '15%'>15%</Dropdown.Item>
+                                <Dropdown.Item eventKey = '18%'>18%</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <button className = 'creature-cut-setting'>설정</button>
+                </form>
             </div>
         </>
     )
