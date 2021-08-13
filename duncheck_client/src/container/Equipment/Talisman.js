cc7f9f18faf141f968331108a94329ad6fa4b3ff
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-13 21:59:40 +0900

    manage state - talisman

--- duncheck_client/src/container/Equipment/Talisman.js
+++ duncheck_client/src/container/Equipment/Talisman.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { settingTalismanCut } from '../../action/TalismanAction'
 
 const Talisman = () => {
+
+    const [checkAllTalisman, setCheckAllTalisman] = useState(false)
+
+    const dispatch = useDispatch()
+
+    const onTalismanCheckHandler = () => {
+        setCheckAllTalisman(!checkAllTalisman)
+    }
+
+    const onSubmitTalismanCheckHandler = (e) => {
+        e.preventDefault()
+        let checkTalisman = {
+            talisman: checkAllTalisman
+        }
+        dispatch(settingTalismanCut(checkTalisman))
+    }
     return (
         <>
             <div className = 'talisman-cut-box'>
                 <div className = 'talisman-cut-title'>탈리스만 컷 설정</div>
-                <span>올 카펠라 체크 여부 : </span>
-                <input type = 'checkbox' className = 'talisman-cut-check' />
-                <button className = 'talisman-cut-setting'>설정</button>
+                <form
+                    onSubmit = {onSubmitTalismanCheckHandler}>
+                    <span>올 카펠라 체크 여부 : </span>
+                    <input type = 'checkbox' className = 'talisman-cut-check' onChange = {onTalismanCheckHandler}/>
+                    <button type = 'submit'>설정</button>
+                </form>
             </div>
         </>
     )
