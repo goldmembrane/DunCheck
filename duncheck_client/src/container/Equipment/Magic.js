d81bab6a85d6a03b696c9c31ab5bb3afc3481054
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-17 20:38:10 +0900

    connect MagicAction.js to Magic.js

--- duncheck_client/src/container/Equipment/Magic.js
+++ duncheck_client/src/container/Equipment/Magic.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { settingWeaponCut, settingShirtCut, settingPantsCut, settingShoulderCut, settingShoesCut, settingArmletCut, settingNecklaceCut, settingRingCut, settingAuxiliaryCut, settingMagicstoneCut, settingEarlingCut } from '../../action/MagicAction'
 
 const Magic = () => {
 
@@ -96,62 +97,76 @@ const Magic = () => {
         let earlingMagic = {
             earling: earling
         }
+        dispatch(settingWeaponCut(weaponMagic))
+        dispatch(settingShirtCut(shirtMagic))
+        dispatch(settingPantsCut(pantsMagic))
+        dispatch(settingShoulderCut(shoulderMagic))
+        dispatch(settingShoesCut(shoesMagic))
+        dispatch(settingArmletCut(armletMagic))
+        dispatch(settingNecklaceCut(necklaceMagic))
+        dispatch(settingRingCut(ringMagic))
+        dispatch(settingAuxiliaryCut(auxiliaryMagic))
+        dispatch(settingMagicstoneCut(magicstoneMagic))
+        dispatch(settingEarlingCut(earlingMagic))
     }
     return (
         <>
             <div className = 'magic-cut-box'>
                 <div className = 'magic-cut-title'>장비 마부 컷 설정</div>
-                <div className = 'magic-cut-container'>
-                    <div className = 'weapon-magic'>
-                        <span>무기 : </span>
-                        <input type = 'number' className = 'weapon-magic-cut' />
+                <form
+                    onSubmit = {onSubmitMagicHandler}>
+                    <div className = 'magic-cut-container'>
+                        <div className = 'weapon-magic'>
+                            <span>무기 : </span>
+                            <input type = 'number' className = 'weapon-magic-cut' onChange = {onWeaponHandler}/>
+                        </div>
+                        <div className = 'shirt-magic'>
+                            <span>상의 : </span>
+                            <input type = 'number' className = 'shirt-magic-cut' onChange = {onShirtHandler}/>
+                        </div>
+                        <div className = 'pants-magic'>
+                            <span>하의 : </span>
+                            <input type = 'number' className = 'pants-magic-cut' onChange = {onPantsHandler}/>
+                        </div>
+                        <div className = 'shoulder-magic'>
+                            <span>어깨 : </span>
+                            <input type = 'number' className = 'shoulder-magic-cut' onChange = {onShoulderHandler}/>
+                        </div>
+                        <div className = 'shoes-magic'>
+                            <span>신발 : </span>
+                            <input type = 'number' className = 'shoes-magic-cut' onChange = {onShoesHandler}/>
+                        </div>
+                        <div className = 'armlet-magic'>
+                            <span>팔찌 : </span>
+                            <input type = 'number' className = 'armlet-magic-cut' onChange = {onArmletHandler}/>
+                        </div>
+                        <div className = 'necklace-magic'>
+                            <span>목걸이 : </span>
+                            <input type = 'number' className = 'necklace-magic-cut' onChange = {onNecklaceHandler}/>
+                        </div>
+                        <div className = 'ring-magic'>
+                            <span>반지 : </span>
+                            <input type = 'number' className = 'ring-magic-cut' onChange = {onRingHandler}/>
+                        </div>
+                        <div className = 'auxiliary-magic'>
+                            <span>보조장비 : </span>
+                            <input type = 'number' className = 'auxiliary-magic-cut' onChange = {onAuxiliaryHandler}/>
+                        </div>
+                        <div className = 'magicstone-magic'>
+                            <span>마법석 : </span>
+                            <input type = 'number' className = 'magicstone-magic-cut' onChange = {onMagicstoneHandler}/>
+                        </div>
+                        <div className = 'earling-magic'>
+                            <span>귀걸이 : </span>
+                            <input type = 'number' className = 'earling-magic-cut' onChange = {onEarlingHandler}/>
+                        </div>
+                        <div className = 'full-critical-check-box'>
+                            <span>만크 여부 : </span>
+                            <input type = 'checkbox' className = 'full-critical-check' />
+                        </div>
                     </div>
-                    <div className = 'shirt-magic'>
-                        <span>상의 : </span>
-                        <input type = 'number' className = 'shirt-magic-cut' />
-                    </div>
-                    <div className = 'pants-magic'>
-                        <span>하의 : </span>
-                        <input type = 'number' className = 'pants-magic-cut' />
-                    </div>
-                    <div className = 'shoulder-magic'>
-                        <span>어깨 : </span>
-                        <input type = 'number' className = 'shoulder-magic-cut' />
-                    </div>
-                    <div className = 'shoes-magic'>
-                        <span>신발 : </span>
-                        <input type = 'number' className = 'shoes-magic-cut' />
-                    </div>
-                    <div className = 'armlet-magic'>
-                        <span>팔찌 : </span>
-                        <input type = 'number' className = 'armlet-magic-cut' />
-                    </div>
-                    <div className = 'necklace-magic'>
-                        <span>목걸이 : </span>
-                        <input type = 'number' className = 'necklace-magic-cut' />
-                    </div>
-                    <div className = 'ring-magic'>
-                        <span>반지 : </span>
-                        <input type = 'number' className = 'ring-magic-cut' />
-                    </div>
-                    <div className = 'auxiliary-magic'>
-                        <span>보조장비 : </span>
-                        <input type = 'number' className = 'auxiliary-magic-cut' />
-                    </div>
-                    <div className = 'magicstone-magic'>
-                        <span>마법석 : </span>
-                        <input type = 'number' className = 'magicstone-magic-cut' />
-                    </div>
-                    <div className = 'earling-magic'>
-                        <span>귀걸이 : </span>
-                        <input type = 'number' className = 'earling-magic-cut' />
-                    </div>
-                    <div className = 'full-critical-check-box'>
-                        <span>만크 여부 : </span>
-                        <input type = 'checkbox' className = 'full-critical-check' />
-                    </div>
-                </div>
-                <button className = 'magic-cut-setting'>설정</button>
+                    <button type = 'submit'>설정</button>
+                </form>
             </div>
         </>
     )
