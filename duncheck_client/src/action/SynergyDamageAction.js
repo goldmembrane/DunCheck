aa6b7a7164a9e00ca9d4532edd3e67252a04dde0
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-09-09 18:07:07 +0900

    delete two files

--- duncheck_client/src/action/SynergyDamageAction.js
+++
@@ -1,8 +0,0 @@
-import { SYNERGY_DAMAGE_CUT } from "./Type"
-
-export const settingSynergyDamageCut = (synergyDamage) => {
-    return {
-        type:  SYNERGY_DAMAGE_CUT,
-        payload: synergyDamage
-    }
-}
